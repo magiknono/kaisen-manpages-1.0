fc61c58820c1b3bc1dff9604790a44485939b442
arnaud <arno.cormier@gmail.com>
2021-02-09 18:48:54 +0100

    kaisen-manpages

---
+++ kaisen-manpages/manpages/doc-versions.js
@@ -0,0 +1,10 @@
+var versionNodes = [
+  {
+    version: "sort by packages", // version number or name (required)
+    url: "http://localhost/manpages/readme.html" // documentation URL (required)
+  },
+  {
+  version: "sort by category", 
+  url: "http://localhost/manpages/category/readme.html"
+  }
+]
